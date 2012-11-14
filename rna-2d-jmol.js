5622ee3921cc70372cc4268b78d373a3e6e0c07b
Blake Sweeney <blakes.85@gmail.com>
2012-11-14 09:25:49 -0500

    Whoops, syntax error

--- rna-2d-jmol.js
+++ rna-2d-jmol.js
@@ -8,7 +8,7 @@ var jmolSetup = function() {
     jmolDiv.html( jmolApplet(500, "", 0) )
     .append('<label><input type="checkbox" id="showNtNums">Nucleotide numbers</label>')
     .append('<input type="button" class="btn" id="neighborhood" value="Show neighborhood">')
-    .append('<input type="button" id="stereo" value="Stereo">');
+    .append('<input type="button" id="stereo" value="Stereo">')
     .show();
   }
 
