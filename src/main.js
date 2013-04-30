8ba6faf1b4e4b2eadc83444a6f70041ccb2891d4
Blake Sweeney <blakes.85@gmail.com>
2013-04-30 13:58:36 -0400

    Minor cleanups.
    The way everything was drawn was a bit odd. We would call a function on the
    selection to do everything. It's easier to just do what we want to the
    selection directly without the need for some callback. This reorganizes things
    like that. Also since the code for the two scales is the same we create a
    function for that.

--- src/main.js
+++ src/main.js
@@ -8,94 +8,86 @@ var Rna2D = window.Rna2D || function(config) {
     // Setup the view
     plot.view.setup();
 
-    d3.select(plot.selection()).call(function(sel) {
+    var margin = plot.margin(),
+        selection = d3.select(plot.selection()),
+        scale = function(domainFn, max) { 
+          return d3.scale.linear().domain(domainFn()).range([0, max]);
+        };
 
-      var margin = plot.margin();
+    // Setup the overall drawing area
+    selection.select('svg').remove();
+    var top = selection.append('svg')
+      .attr('width', plot.width() + margin.left + margin.right)
+      .attr('height', plot.height() + margin.above + margin.below);
 
-      sel.select('svg').remove();
-      var top = sel.append('svg')
-            .attr('width', plot.width() + margin.left + margin.right)
-            .attr('height', plot.height() + margin.above + margin.below);
+    plot.vis = top.append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.above + ")");
 
-      plot.vis = top.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.above + ")");
+    // Setup the scales
+    plot.xScale(scale(plot.xDomain, plot.width() - margin.right));
+    plot.yScale(scale(plot.yDomain, plot.height() - margin.above));
 
-      // ----------------------------------------------------------------------
-      // Setup the axis
-      // ----------------------------------------------------------------------
-      plot.xScale(d3.scale.linear().domain(plot.xDomain()).range([0, plot.width() - margin.right]));
-      plot.yScale(d3.scale.linear().domain(plot.yDomain()).range([0, plot.height() - margin.above]));
+    // Generate the components - brush, frame, zoom, etc
+    plot.components();
 
-      // ----------------------------------------------------------------------
-      // Generate the components - brush, frame, zoom, etc
-      // ----------------------------------------------------------------------
-      plot.components();
+    // Draw all coordinates and attach all standard data
+    plot.coordinates(function(selection) {
 
-      // ----------------------------------------------------------------------
-      // Draw all coordinates and attach all standard data
-      // ----------------------------------------------------------------------
-      plot.coordinates(function(selection) {
+      var x = plot.views[plot.view()].xCoord(),
+          y = plot.views[plot.view()].yCoord(),
+          classOf = plot.nucleotides.classOf(),
+          klass = plot.nucleotides['class']();
 
-          var x = plot.views[plot.view()].xCoord(),
-              y = plot.views[plot.view()].yCoord(),
-              classOf = plot.nucleotides.classOf(),
-              klass = plot.nucleotides['class']();
+      selection.attr('id', plot.nucleotides.getID())
+        .attr('class', function(d, i) { return classOf(d, i).concat(klass).join(' '); })
+        .datum(function(d, i) {
+          d.__x = x(d, i);
+          d.__y = y(d, i);
+          return d;
+        })
+        .attr('visibility', plot.nucleotides.visiblity)
+        .attr('data-sequence', plot.nucleotides.getSequence());
 
-        selection.attr('id', plot.nucleotides.getID())
-          .attr('class', function(d, i) { return classOf(d, i).concat(klass).join(' '); })
-          .datum(function(d, i) {
-            d.__x = x(d, i);
-            d.__y = y(d, i);
-            return d;
-          })
-          .attr('visibility', plot.nucleotides.visiblity)
-          .attr('data-sequence', plot.nucleotides.getSequence());
+      Rna2D.utils.attachHandlers(selection, plot.nucleotides);
 
-        Rna2D.utils.attachHandlers(selection, plot.nucleotides);
-
-        return selection;
-      });
-
-      // ----------------------------------------------------------------------
-      // Draw all interactions and add all common data
-      // ----------------------------------------------------------------------
-      plot.connections(function(selection) {
-        var ntsOf = plot.interactions.getNTs(),
-            classOf = plot.interactions.classOf(),
-            klass = plot.interactions['class']();
-
-        selection.attr('id', plot.interactions.getID())
-          .attr('class', function(d, i) { return classOf(d, i).concat(klass).join(' '); })
-          .attr('visibility', plot.interactions.visibilityString)
-          .attr('data-nts', function(d, i) { return ntsOf(d).join(','); })
-          .attr('nt1', function(d, i) { return ntsOf(d)[0]; })
-          .attr('nt2', function(d, i) { return ntsOf(d)[1]; });
-
-        Rna2D.utils.attachHandlers(selection, plot.interactions);
-
-        return selection;
-      });
-
-      // ----------------------------------------------------------------------
-      // Draw motifs
-      // ----------------------------------------------------------------------
-      plot.groups(function(selection) {
-        var ntsOf = plot.motifs.getNTs(),
-            classOf = plot.motifs.classOf(),
-            klass = plot.motifs['class']();
-
-        selection.attr('id', plot.motifs.getID())
-          .attr('class', function(d, i) { return classOf(d, i).concat(klass).join(' '); })
-          .attr('data-nts', function(d) { return plot.motifs.getNTs()(d).join(','); })
-          .attr('visibility', plot.motifs.visibility);
-
-        Rna2D.utils.attachHandlers(selection, plot.motifs);
-
-        return selection;
-      });
-
-      return plot;
+      return selection;
     });
+
+    // Draw all interactions and add all common data
+    plot.connections(function(selection) {
+      var ntsOf = plot.interactions.getNTs(),
+          classOf = plot.interactions.classOf(),
+          klass = plot.interactions['class']();
+
+      selection.attr('id', plot.interactions.getID())
+        .attr('class', function(d, i) { return classOf(d, i).concat(klass).join(' '); })
+        .attr('visibility', plot.interactions.visibilityString)
+        .attr('data-nts', function(d, i) { return ntsOf(d).join(','); })
+        .attr('nt1', function(d, i) { return ntsOf(d)[0]; })
+        .attr('nt2', function(d, i) { return ntsOf(d)[1]; });
+
+      Rna2D.utils.attachHandlers(selection, plot.interactions);
+
+      return selection;
+    });
+
+    // Draw motifs
+    plot.groups(function(selection) {
+      var ntsOf = plot.motifs.getNTs(),
+          classOf = plot.motifs.classOf(),
+          klass = plot.motifs['class']();
+
+      selection.attr('id', plot.motifs.getID())
+        .attr('class', function(d, i) { return classOf(d, i).concat(klass).join(' '); })
+        .attr('data-nts', function(d) { return plot.motifs.getNTs()(d).join(','); })
+        .attr('visibility', plot.motifs.visibility);
+
+      Rna2D.utils.attachHandlers(selection, plot.motifs);
+
+      return selection;
+    });
+
+    return plot;
   };
 
   // Configure the plot
