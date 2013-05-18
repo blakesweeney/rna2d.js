86a49964e8cf35fd6c78cec5aa91d45b7337e423
Blake Sweeney <blakes.85@gmail.com>
2013-05-18 16:30:10 -0400

    Update chains
    Update the config functions for accessing chain data.

--- src/components/chain.js
+++ src/components/chain.js
@@ -1,12 +1,31 @@
 Rna2D.components.chains = (function () {
+
   return {
     dataStore: true,
-    config: {
-      getID: function(d, i) { return d.id; },
-      'class': 'chain',
-      classOf: function(d, i) { return []; },
-      getNTs: function(d, i) { return d.nts; }
-    }
+    config: function() {
+      return {
+        getID: function(d, i) { return d.id; },
+        'class': 'chain',
+        classOf: function(d, i) { return []; },
+        getNTData: function(d, i) { return d.nts; },
+        chainOf: function(d, i) {
+          var ntsOf = plot.chains.getNTData(),
+              chainIndex = -1;
+          $.each(plot.chains(), function(chain, index) {
+            if (ntsOf(chain)[i] === d) {
+              chainIndex = index;
+            }
+          });
+          return chainIndex;
+        }
+      };
+    },
+
+    sideffects: function() { },
+
+    actions: function() {}
+
   };
+
 }());
 
