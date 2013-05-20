0c3382fa3163da45159e3af7b3ae985016235b41
Blake Sweeney <blakes.85@gmail.com>
2013-05-20 09:32:52 -0400

    Modify chainOf to look by string id or data
    chainOf will no accept either the data object to check or the id of the
    nucleotide we are looking for.

--- src/components/chain.js
+++ src/components/chain.js
@@ -14,9 +14,18 @@ Rna2D.components.chains = (function () {
     sideffects: function(plot) { 
       plot.chains.chainOf = function(d, i) {
         var ntsOf = plot.chains.getNTData(),
-            chainIndex = -1;
+            chainIndex = -1,
+            compare = function(d, i, chain) { return ntsOf(chain)[i] === d; };
+
+          if (typeof(d) === "string") {
+            var idOf = plot.nucleotides.getID();
+            compare = function(d, i, chain) {
+              return idOf(ntsOf(chain)[i]) === d;
+            };
+          }
+
         $.each(plot.chains(), function(index, chain) {
-          if (ntsOf(chain)[i] === d) {
+          if (compare(d, i, chain)) {
             chainIndex = index;
           }
         });
