1d74369bd65e9b01b44cbc367ecf2b33be81ba9f
Blake Sweeney <blakes.85@gmail.com>
2013-08-01 09:55:55 -0400

    Update pure jmol interface
    The pure jmol interface needs to use my new oo component based interface. This
    updates that component to use the correct interface. It is not yet tested
    properly, but it is a start.

--- rna2d.js
+++ rna2d.js
@@ -619,100 +619,91 @@ Rna2D.components.interactions = function(plot) {
   return interactions;
 };
 
-Rna2D.components.jmol = {
+Rna2D.components.jmol = function(plot) {
 
-  config: function(plot) {
-    return {
-      enable: false,
-      divID: 'jmol',
-      file: 'static/jmol/data/2AVY.pdb',
-      showOnStartup: true,
-      postSetup: Object
-    };
-  },
+  var loaded = false,
+      showNTGroup = function(type) {
+        return function(d, i) {
+          var numberOf = plot.nucleotides.getNumber(),
+              chainOf = plot.nucleotides.getChain(),
+              nts = type.nucleotides(d, i),
+              data = [];
 
-  actions: function(plot) {
-    var loaded = false,
-        showNTGroup = function(type) {
-          return function(d, i) {
-            var numberOf = plot.nucleotides.getNumber(),
-                chainOf = plot.nucleotides.getChain(),
-                nts = type.nucleotides(d, i),
-                data = [];
+          nts.datum(function(d) {
+            data.push({number: numberOf(d), chain: chainOf(d)});
+            return d;
+          });
 
-            nts.datum(function(d) {
-              data.push({number: numberOf(d), chain: chainOf(d)});
-              return d;
-            });
-
-            return plot.jmol.showNTs(data);
-          };
+          return plot.jmol.showNTs(data);
         };
+      };
 
-    plot.jmol.nucleotides = function(d, i) {
-        var numberOf = plot.nucleotides.getNumber(),
-            chainOf = plot.nucleotides.getChain();
-        return plot.jmol.showNTs([{number: numberOf(d), chain: chainOf(d)}]);
-    };
-
-    plot.jmol.interactions = showNTGroup(plot.interactions);
-    plot.jmol.motifs = showNTGroup(plot.motifs);
-
-    plot.jmol.brush = function(data) {
+  var Jmol = inhert(Rna2D.Component, 'jmol', {
+    divID: 'jmol',
+    file: 'static/jmol/data/2AVY.pdb',
+    showOnStartup: true,
+    postSetup: Object,
+    render: false,
+    nucleotides: function(d, i) {
+      var numberOf = plot.nucleotides.getNumber(),
+          chainOf = plot.nucleotides.getChain();
+      return plot.jmol.showNTs([{number: numberOf(d), chain: chainOf(d)}]);
+    },
+    interactions: showNTGroup(plot.interactions),
+    motifs: showNTGroup(plot.motifs),
+    brush: function(data) {
       var numberOf = plot.nucleotides.getNumber(),
           chainOf = plot.nucleotides.getChain();
       return plot.jmol.showNTs($.map(data, function(d) {
         return {number: numberOf(d), chain: chainOf(d)};
       }));
-    };
+    },
+  });
 
-    plot.jmol.setup = function() {
-      if (loaded) {
-        return true;
-      }
+  Jmol.prototype.draw = function() {
+    return (plot.jmol.showOnStartup() ? this.setup() : true);
+  };
 
-      jmolScript("load " + plot.jmol.file() + ";");
-      loaded = true;
-      plot.jmol.postSetup()();
+  var jmol = new Jmol();
+
+  jmol.setup = function() {
+    if (loaded) {
       return true;
-    };
-
-    plot.jmol.showNTs = function(ids) {
-      var commands = [],
-          ntSelect = [];
-
-      $.each(ids, function(index, data) {
-        ntSelect.push(data.number + ':' + data.chain);
-      });
-
-      ntSelect = ntSelect.join(' or ');
-      commands.push('select ' + ntSelect + ';');
-      commands.push('show ' + ntSelect + ';');
-
-      return plot.jmol.run(commands);
-    };
-
-    plot.jmol.run = function(commands) {
-      plot.jmol.setup();
-
-      if (typeof(commands) !== 'string') {
-        commands = commands.join("\n");
-      }
-
-      return jmolScript(commands);
-    };
-  },
-
-  generate: function(plot) {
-    if (!plot.jmol.enable()) {
-      return false;
     }
 
-    if (plot.jmol.showOnStartup()) {
-      plot.jmol.setup();
-    }
+    jmolScript("load " + this.file() + ";");
+    loaded = true;
+    this.postSetup()();
     return true;
-  }
+  };
+
+  jmol.showNTs = function(ids) {
+    var commands = [],
+        ntSelect = [];
+
+    $.each(ids, function(index, data) {
+      ntSelect.push(data.number + ':' + data.chain);
+    });
+
+    ntSelect = ntSelect.join(' or ');
+    commands.push('select ' + ntSelect + ';');
+    commands.push('show ' + ntSelect + ';');
+
+    return jmol.run(commands);
+  };
+
+  jmol.run = function(commands) {
+    this.setup();
+
+    if (typeof(commands) !== 'string') {
+      commands = commands.join("\n");
+    }
+
+    return jmolScript(commands);
+  };
+
+  jmol.attach(plot);
+  return jmol;
 
 };
 
